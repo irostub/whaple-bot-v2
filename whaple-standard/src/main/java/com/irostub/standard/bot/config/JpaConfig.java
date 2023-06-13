e8ba3d7def7df72abda45fab3631e953b1927c1a
irostub <irostub@gmail.com>
2023-06-13 23:46:59 +0900

    Fix multi module dependency

--- whaple-standard/src/main/java/com/irostub/standard/bot/config/JpaConfig.java
+++ whaple-standard/src/main/java/com/irostub/standard/bot/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.irostub.standard.bot.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @ComponentScan("com.irostub.domain")
 @EnableJpaRepositories("com.irostub.domain")
+@EntityScan("com.irostub.domain")
 public class JpaConfig {
 }
