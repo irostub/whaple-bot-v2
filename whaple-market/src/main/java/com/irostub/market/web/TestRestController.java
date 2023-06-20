271e5ba3550cd742051677048d19e9da1ffb8883
irostub <irostub@gmail.com>
2023-06-21 01:09:52 +0900

    implement event

--- whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
+++ whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
@@ -21,22 +21,36 @@ import java.util.stream.Collectors;
 @RestController
 @CrossOrigin("*")
 public class TestRestController {
-    public static String hmacAndHex(String secret, String data, String Algorithms) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+    public static byte[] hmacAndHex(String secret, String data, String Algorithms) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
 
-        byte[] hash = getBytes(secret, data, Algorithms);
+        return getBytes(secret, data, Algorithms);
 
         //4. Hex Encode to String
-        return Hex.encodeHexString(hash);
+//        return Hex.encodeHexString(hash);
     }
+    public static byte[] hmacAndHex(byte[] secret, String data, String Algorithms) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
 
-    public static void main(String[] args) {
-        String s = Hex.encodeHexString("hello".getBytes());
-        System.out.println(s);
+        return getBytes(secret, data, Algorithms);
+
+        //4. Hex Encode to String
+//        return Hex.encodeHexString(hash);
+    }
+    public static byte[] getBytes(byte[] secret, String data, String Algorithms) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
+        //1. SecretKeySpec 클래스를 사용한 키 생성
+        SecretKeySpec secretKey = new SecretKeySpec(secret, Algorithms);
+        //2. 지정된  MAC 알고리즘을 구현하는 Mac 객체를 작성합니다.
+        Mac hasher = Mac.getInstance(Algorithms);
+
+        //3. 키를 사용해 이 Mac 객체를 초기화
+        hasher.init(secretKey);
+
+        //3. 암호화 하려는 데이터의 바이트의 배열을 처리해 MAC 조작을 종료
+        byte[] hash = hasher.doFinal(data.getBytes());
+        return hash;
     }
     public static byte[] getBytes(String secret, String data, String Algorithms) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
         //1. SecretKeySpec 클래스를 사용한 키 생성
         SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("utf-8"), Algorithms);
-
         //2. 지정된  MAC 알고리즘을 구현하는 Mac 객체를 작성합니다.
         Mac hasher = Mac.getInstance(Algorithms);
 
@@ -58,13 +72,12 @@ public class TestRestController {
         log.info("collect={}", collect);
 
 
-        String secret = hmacAndHex(Hex.encodeHexString("WebAppData".getBytes()), token, "HmacSHA256");
-        String hmacSHA256 = hmacAndHex(secret, collect, "HmacSHA256");
-        log.info(secret);
-        log.info(hmacSHA256);
+        byte[] secret = hmacAndHex("WebAppData", token, "HmacSHA256");
+        byte[] hmacSHA256s = hmacAndHex(secret, collect, "HmacSHA256");
+
         log.info(body.hash);
-        log.info(Hex.encodeHexString(body.hash.getBytes()));
-        if (hmacSHA256.equals(body.hash)) {
+        log.info(Hex.encodeHexString(hmacSHA256s));
+        if (Hex.encodeHexString(hmacSHA256s).equals(body.hash)) {
             log.info("success");
             return ResponseEntity.ok().build();
         }
