0a346d3687414ccc9dede18e00a86e29c6b2ae1f
irostub <irostub@gmail.com>
2023-06-21 21:53:40 +0900

    implement event

--- whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
+++ whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
@@ -1,7 +1,6 @@
 package com.irostub.market.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -21,62 +21,28 @@ import java.util.stream.Collectors;
 @RestController
 @CrossOrigin("*")
 public class TestRestController {
-    public static byte[] hmacAndHex(String secret, String data, String Algorithms) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-
-        return getBytes(secret, data, Algorithms);
-
-        //4. Hex Encode to String
-//        return Hex.encodeHexString(hash);
-    }
-    public static byte[] hmacAndHex(byte[] secret, String data, String Algorithms) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-
-        return getBytes(secret, data, Algorithms);
-
-        //4. Hex Encode to String
-//        return Hex.encodeHexString(hash);
-    }
-    public static byte[] getBytes(byte[] secret, String data, String Algorithms) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-        //1. SecretKeySpec 클래스를 사용한 키 생성
-        SecretKeySpec secretKey = new SecretKeySpec(secret, Algorithms);
-        //2. 지정된  MAC 알고리즘을 구현하는 Mac 객체를 작성합니다.
-        Mac hasher = Mac.getInstance(Algorithms);
-
-        //3. 키를 사용해 이 Mac 객체를 초기화
-        hasher.init(secretKey);
-
-        //3. 암호화 하려는 데이터의 바이트의 배열을 처리해 MAC 조작을 종료
-        byte[] hash = hasher.doFinal(data.getBytes());
-        return hash;
-    }
-    public static byte[] getBytes(String secret, String data, String Algorithms) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-        //1. SecretKeySpec 클래스를 사용한 키 생성
-        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("utf-8"), Algorithms);
-        //2. 지정된  MAC 알고리즘을 구현하는 Mac 객체를 작성합니다.
-        Mac hasher = Mac.getInstance(Algorithms);
-
-        //3. 키를 사용해 이 Mac 객체를 초기화
-        hasher.init(secretKey);
-
-        //3. 암호화 하려는 데이터의 바이트의 배열을 처리해 MAC 조작을 종료
-        byte[] hash = hasher.doFinal(data.getBytes());
-        return hash;
-    }
 
     @PostMapping("/test/btn")
     public ResponseEntity<?> testBtn(@RequestBody Data body, @Value("${app.bot.token}") String token) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
         String[] split = body.getToken().split("&");
-        String collect = Arrays.stream(split)
+        String dataCheckString = Arrays.stream(split)
                 .sorted()
                 .collect(Collectors.joining("\n"));
-        log.info("collect={}", collect);
+        log.info("dataCheckString={}", dataCheckString);
         String hash = Arrays.stream(split).filter(s -> s.startsWith("hash")).findFirst().get();
+        Mac hmacSha256 = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec("WebAppData".getBytes(), "HmacSHA256");
+        hmacSha256.init(secret_key);
+        byte[] firstKey = hmacSha256.doFinal(token.getBytes());
 
-        byte[] secret = hmacAndHex("WebAppData", token, "HmacSHA256");
-        byte[] hmacSHA256s = hmacAndHex(secret, collect, "HmacSHA256");
-
+        Mac newMacSha256 = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key2 = new SecretKeySpec(firstKey, "HmacSHA256");
+        newMacSha256.init(secret_key2);
+        byte[] bytes = newMacSha256.doFinal(dataCheckString.getBytes());
+        String hex = String.format("%040x", new BigInteger(1, bytes));
         log.info(hash);
-        log.info(Hex.encodeHexString(hmacSHA256s));
-        if (Hex.encodeHexString(hmacSHA256s).equals(hash)) {
+        log.info(hex);
+        if (hex.equals(hash)) {
             log.info("success");
             return ResponseEntity.ok().build();
         }
