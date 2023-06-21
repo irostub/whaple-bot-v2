0d6073d139a868a70d8d5d29acbbddda6449b507
irostub <irostub@gmail.com>
2023-06-22 00:45:36 +0900

    implement event

--- whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
+++ whaple-market/src/main/java/com/irostub/market/web/TestRestController.java
@@ -13,7 +13,6 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -23,31 +22,24 @@ import java.util.stream.Collectors;
 @RestController
 @CrossOrigin("*")
 public class TestRestController {
-
     @PostMapping("/test/btn")
     public ResponseEntity<?> testBtn(@RequestBody Data body, @Value("${app.bot.token}") String token) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-        log.info(body.getToken());
-        String decode = URLDecoder.decode(body.getToken(), StandardCharsets.UTF_8).replaceAll("\"", "");
-        log.info(decode);
-        String[] split = decode.split("&");
-        String dataCheckString = Arrays.stream(split)
-                .sorted()
-                .collect(Collectors.joining("\n"));
-        log.info("dataCheckString={}", dataCheckString);
-        String hash = Arrays.stream(split).filter(s -> s.startsWith("hash")).findFirst().get();
+        String dataValid = Arrays.stream(URLDecoder.decode(body.getToken(), "UTF-8").split("&")).filter(s -> !s.startsWith("hash=")).sorted().collect(Collectors.joining("\n"));
+        String hash = Arrays.stream(URLDecoder.decode(body.getToken(), "UTF-8").split("&")).filter(s -> s.startsWith("hash=")).findFirst().get().substring(5);
+
         Mac hmacSha256 = Mac.getInstance("HmacSHA256");
-        SecretKeySpec secret_key = new SecretKeySpec("WebAppData".getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec("WebAppData".getBytes("UTF-8"), "HmacSHA256");
         hmacSha256.init(secret_key);
-        byte[] firstKey = hmacSha256.doFinal(token.getBytes(StandardCharsets.UTF_8));
+        byte[] firstKey = hmacSha256.doFinal(token.getBytes("UTF-8"));
 
         Mac newMacSha256 = Mac.getInstance("HmacSHA256");
         SecretKeySpec secret_key2 = new SecretKeySpec(firstKey, "HmacSHA256");
         newMacSha256.init(secret_key2);
-        byte[] bytes = newMacSha256.doFinal(dataCheckString.getBytes(StandardCharsets.UTF_8));
-        String hex = Hex.encodeHexString(bytes);
-        log.info(hash);
-        log.info(hex);
-        if (hex.equals(hash.substring(5))) {
+        byte[] bytes = newMacSha256.doFinal(dataValid.getBytes("UTF-8"));
+
+        String s = Hex.encodeHexString(bytes);
+
+        if (s.equals(hash)) {
             log.info("success");
             return ResponseEntity.ok().build();
         }
