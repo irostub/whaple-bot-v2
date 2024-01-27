7fc7bbf94d2833cf53355ddba4e4bb874b7bafee
iro <irostub@gmail.com>
2024-01-28 03:30:56 +0900

    Set timezone

--- whaple-standard/src/main/java/com/irostub/standard/StandardApplication.java
+++ whaple-standard/src/main/java/com/irostub/standard/StandardApplication.java
@@ -7,12 +7,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class StandardApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(StandardApplication.class, args);
     }
 }
